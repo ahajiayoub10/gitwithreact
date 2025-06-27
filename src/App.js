f2cebf98a590c52df96ee5f262b82f5416bf66b3
ayoub <ayoub10ahaji@gmail.com>
2025-06-27 19:18:23 +0100

    Project react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Bonjour Ayoub 👋</h1>
+      <p>Ceci est mon premier projet React.</p>
+    </div>
+  );
+}
+export default App;
+
+export default App;
